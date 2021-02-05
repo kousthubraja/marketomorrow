5ec0d7dc0834c1f00e38f7b9295beb7fdf21b99b
Kousthub <kousthub.raja@gmail.com>
2021-02-05 14:20:34 +0530

    Change path

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/nifty-prediction', function(req, res, next) {
   res.render('index', { color: '#00b44c' });
 });
 
