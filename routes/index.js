c7836594882edd2a5ff76663a3151407a399fe43
Kousthub <kousthub.raja@gmail.com>
2021-02-05 14:09:56 +0530

    Text change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Marketomorrow',  color: '#00b44c' });
+  res.render('index', { color: '#00b44c' });
 });
 
 module.exports = router;
