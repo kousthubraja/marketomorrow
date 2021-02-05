676b2a001d84f996b96bd0777e628b6c2dc912d7
Lakshay Dahiya <lakshay14057@iiitd.ac.in>
2021-02-05 21:12:29 +0530

    Changes to Nifty-prediction site

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/nifty-prediction', function(req, res, next) {
-  res.render('index', { color: '#00b44c' });
+  // res.render('index', { color: '#00b44c' });
+  res.render('index', { color: '#b41200' });
 });
 
 module.exports = router;
