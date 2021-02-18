df96f42277930296fecc3e5763d53e251caf2bd4
Lakshay Dahiya <lakshay14057@iiitd.ac.in>
2021-02-18 12:37:31 +0530

    CSS changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use('/', indexRouter);
 app.use("/nifty-prediction/stylesheets", express.static(path.join(__dirname, 'public/stylesheets')));
 app.use("/nifty-prediction/images", express.static(path.join(__dirname, 'public/images')));
