f18e8621920d32913f23937b05a722e3c29c287b
Lakshay Dahiya <lakshay14057@iiitd.ac.in>
2021-02-18 14:20:49 +0530

    Redirect functionality for trailing slash error

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,16 @@ var router = express.Router();
 const myAPI = require('./api');
 
 /* GET home page. */
-router.get('/nifty-prediction', function(req, res, next) {
-  // res.render('index', data);
-  // res.render('index', data);
-  myAPI.getNiftyPrediction(req,res)
-});
+router.get('/nifty-prediction', function (req, res) {
+    // Redirect if no slash at the end
+    if (!req.url.endsWith('/'))
+        res.redirect(301, req.url + '/')
+    else
+        myAPI.getNiftyPrediction(req, res)
 
+    // Normal response goes here
+});
+// router.get('/nifty-prediction', function(req, res, next) {
+
+// });
 module.exports = router;
